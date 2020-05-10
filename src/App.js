b46a5e5716410bf82d794f5c7d3a4c4a730214b0
raopg <prateek.rao.08@gmail.com>
2020-05-09 19:56:14 -0700

    Initial set up tablelist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {State} from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import { TableList } from 'react-viewerbase';
+import { TableList, TableListItem } from 'react-viewerbase';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <TableList headerList = "Example Table List Header">
-        {/* Add TableListItem in here*/}
-      </TableList>
+      <State
+        initial={{
+          selectedIndex: null,
+          listItems: [
+            { label: 'Brain Left' },
+            { label: 'Brain Right' },
+            { label: 'Heart Left' },
+            { label: 'Heart Right' },
+          ],
+        }}>
+          {({ state, setState }) => (
+            <TableList headerTitle="Example Table List Header">
+              {state.listItems.map((item, index) => {
+                return (
+                  <TableListItem
+                    key={`item_${index}`}
+                    itemClass={state.selectedIndex === index ? 'selected' : ''}
+                    itemIndex={index}
+                    onItemClick={() => alert('item clicked')}
+                  >
+                    <label>{item.label}</label>
+                  </TableListItem>
+                )
+              })}
+            </TableList>
+        )}
+        </State>
+      
     </div>
   );
 }
